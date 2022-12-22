013d457153d1c78b40a019aa9443a970f823f8b5
Amiya kiran <amiya.susy@gmail.com>
2022-12-22 17:23:41 +0530

    contact manager v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ContactManagerComponent } from './contact-manager/contact-manager.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UpdateContactComponent } from './update-contact/update-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // when someone request localhost4200('') then redirect to the given path('/contacts/admin=>path of ContactManagerComponent) and set pathmatch full do the work
+  {path: '', redirectTo : '/contacts/admin', pathMatch:'full'},
+  
+  // path for ContactManagerComponent
+  { path:'contacts/admin', component:ContactManagerComponent},
+  
+  // path for AddContactComponent
+  { path: 'contacts/add', component:AddContactComponent},
+  
+  //path for UpdateContactComponent => :parametername is used to indicate which contact as to be edited
+  { path: 'contacts/edit/:contactId', component : UpdateContactComponent},
+  
+  //path for ViewContactComponent
+   {path: 'contacts/view/:contactId', component : ViewContactComponent},
+   
+   //path for PageNotFoundComponent => ** indicate those pages(url) are not the part of this project
+   {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MyContact } from 'src/models/myContact';
+import { Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  baseUrl:string = 'http://localhost:3000/contacts'
+
+  constructor(private http:HttpClient) { }
+
+  //api call for get allcontacts-function
+
+  getAllContacts():Observable<MyContact>{
+     return this.http.get(this.baseUrl) 
+  }
+
+  //function for view a particular contact
+  viewContact(contactId:string){
+    return this.http.get(`${this.baseUrl}/${contactId}`)
+  }
+  //Function for getting a particular groupname
+   getGroupName(groupId:string){
+   return this.http.get('http://localhost:3000/groups/'+groupId)
+  }
+  //function for fetch all groups from http://localhost:3000/groups
+  getAllGroups(){
+    return this.http.get('http://localhost:3000/groups')
+  }
+
+  // function for Adding new contact to server
+    addContact(contactBody:any){
+    return  this.http.post(this.baseUrl, contactBody)
+  }
+  
+}
+
